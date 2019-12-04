06446a3680ed5b3e26d45c5d912afc168404e6bc
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-04 22:58:22 +0530

    added Home view

--- api_service/views.py
+++ api_service/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def home_view(request):
+    return JsonResponse({'success': True, 'message': 'Please view documentation at https://docs.google.com/document/d/17DkpOB_B37Zc5SlaAGtWiqZde1gTfLPqpQKdDbKvjgE/edit?usp=sharing'})
