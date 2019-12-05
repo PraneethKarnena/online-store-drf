a6c52b0b516b7dda212311be5bb22cd516685207
Praneeth Karnena <praneeth.codes@gmail.com>
2019-12-05 22:54:46 +0530

    configured for Heroku

--- online_store/settings.py
+++ online_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku # required for Heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+
+django_heroku.settings(locals()) # For Heroku
